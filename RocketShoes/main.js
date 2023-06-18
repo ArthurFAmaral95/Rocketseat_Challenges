5ad1d07e40674b480132a59aff43be227c5fc828
Arthur Amaral <arthuramaral6@gmail.com>
2023-06-18 18:34:46 -0300

    rocketshoes: changing big image on click

--- RocketShoes/main.js
+++ RocketShoes/main.js
@@ -1,11 +1,12 @@
 const smallImgs = document.querySelectorAll('.small-picture img')
+const bigImg = document.querySelector('#big-picture')
 
 function toggleSelectedImg(e) {
   smallImgs.forEach(img => img.classList.remove('selected'))
 
   e.target.classList.add('selected')
 
-
+  bigImg.src = e.target.src
 }
 
 smallImgs.forEach(img => img.addEventListener('click', toggleSelectedImg))
