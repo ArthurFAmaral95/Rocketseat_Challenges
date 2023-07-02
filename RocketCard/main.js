0cf75e963c49541d9310bb1a551ff1e8e18786c2
Arthur Amaral <arthuramaral6@gmail.com>
2023-07-02 16:25:49 -0300

    rocketcard: avatar

--- RocketCard/main.js
+++ RocketCard/main.js
@@ -1,9 +1,11 @@
 const url = 'https://api.github.com/users/ArthurFAmaral95'
 const userLogin = document.querySelector('#userLogin')
+const avatar = document.querySelector('#avatar')
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data)
     userLogin.textContent = data.login
+    avatar.src = data.avatar_url
   })
