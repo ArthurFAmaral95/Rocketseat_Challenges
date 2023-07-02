3187a63fb200404466bb4149652981315afde5cc
Arthur Amaral <arthuramaral6@gmail.com>
2023-07-02 16:55:25 -0300

    rocketcard: infos

--- RocketCard/main.js
+++ RocketCard/main.js
@@ -1,11 +1,20 @@
 const url = 'https://api.github.com/users/ArthurFAmaral95'
 const userLogin = document.querySelector('#userLogin')
 const avatar = document.querySelector('#avatar')
+const followers = document.querySelector('#followers')
+const following = document.querySelector('#following')
+const repos = document.querySelector('#repos')
+const company = document.querySelector('#company')
+const place = document.querySelector('#location')
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
     userLogin.textContent = data.login
     avatar.src = data.avatar_url
+    followers.textContent = `${data.followers} Followers`
+    following.textContent = `${data.following} Following`
+    repos.textContent = `${data.public_repos} Repositories`
+    company.textContent = data.company
+    place.textContent = data.location
   })
