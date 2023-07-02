cc04cc2e8c3e15e901117bfe7971c5e3b3f5db64
Arthur Amaral <arthuramaral6@gmail.com>
2023-07-02 18:14:04 -0300

    rocketcard: change background color

--- RocketCard/main.js
+++ RocketCard/main.js
@@ -6,6 +6,8 @@ const following = document.querySelector('#following')
 const repos = document.querySelector('#repos')
 const company = document.querySelector('#company')
 const place = document.querySelector('#location')
+const btns = document.querySelectorAll('.buttons button')
+const bgCard = document.querySelector('.bgCard')
 
 fetch(url)
   .then(response => response.json())
@@ -18,3 +20,22 @@ fetch(url)
     company.textContent = data.company
     place.textContent = data.location
   })
+
+function randomNumber(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min
+  return num
+}
+
+function randomRGB() {
+  return `rgb(${randomNumber(0, 255)},${randomNumber(0, 255)},${randomNumber(
+    0,
+    255
+  )})`
+}
+
+function handleClick(e) {
+  console.log(e.target.id)
+  bgCard.style.backgroundColor = randomRGB()
+}
+
+btns.forEach(btn => btn.addEventListener('click', handleClick))
