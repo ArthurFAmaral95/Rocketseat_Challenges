76e96badfe4647a9423a4bf69283c56d9e8f3a95
Arthur Amaral <arthuramaral6@gmail.com>
2023-07-04 21:23:53 -0300

    rocketflix: api.js

---
+++ Rocketflix/api.js
@@ -0,0 +1,7 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+//https://www.themoviedb.org/signup
+
+export const API_KEY = 'MY_API_KEY'
+export const ACESS_TOKEN = 'MY_ACESS_TOKEN'
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+export const language = 'language=pt-BR'
