665fc259e776c0387e60ca89f9fd92e0d16bc093
Arthur Amaral <arthuramaral6@gmail.com>
2023-07-12 22:18:10 -0300

    rocketflix: event listener

--- Rocketflix/main.js
+++ Rocketflix/main.js
@@ -1,6 +1,31 @@
 import * as APIData from './api.js'
 
-function randomNumber(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min
+const btn = document.querySelector('button')
+
+function randomNumber() {
+  const num = Math.floor(Math.random() * 10) + 1
   return num
 }
+
+btn.addEventListener('click', () => {
+  const page = randomNumber()
+  const movie = randomNumber()
+
+  console.log({ page, movie })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${APIData.ACESS_TOKEN}`
+    }
+  }
+
+  fetch(
+    `${APIData.BASE_URL}discover/movie?include_adult=false&include_video=false&${APIData.language}&page=${page}&sort_by=popularity.desc`,
+    options
+  )
+    .then(response => response.json())
+    .then(response => console.log(response.results[movie]))
+    .catch(err => console.error(err))
+})
